51108714b4a15b26492b233cb43c8d479b154644
Siripond <goog.mullanu@gmail.com>
2021-03-03 09:34:20 +1100

    Update validation

--- routes/signup.js
+++ routes/signup.js
@@ -1,19 +1,34 @@
 const express = require('express')
 const router = express.Router();
 const pool = require('../database')
+const validator = require('validator');
+const { render } = require('ejs');
 
 //--Sign up
 router.get('/', (req, res) => {
     res.render('pages/signup', {
-        message: ""
-    })
+        NavEnabled: false,
+        ErrMsgEnabled: false,
+        errMsg: ""
+    });
 });
 router.post('/', (req, res) => {
     try {
         const { firstname, lastname, email, password, confirmPassword } = req.body;
+        let errMsg = ""
+
+        // Finds the validation errors
+        if (!validator.isEmail(email)) {
+            errMsg = "Please enter email in correct format"
+        } else if (!validator.isStrongPassword(password)) {
+            errMsg = "Please enter password in correct format"
+        } else if (password != confirmPassword) {
+            // Password and Confirm password does not match
+            errMsg = "Password doesn't match"
+        }
 
         // Password and Confirm Password validation
-        if (password === confirmPassword) {
+        if (errMsg === "") {
             // Registered email validation
             let sql = "SELECT * FROM users WHERE email = '" + email + "'"
             pool.query(sql, (error, results) => {
@@ -22,9 +37,7 @@ router.post('/', (req, res) => {
                 } else {
                     if (results.rows.length > 0) {
                         // This email was registered
-                        res.render('pages/signup', {
-                            message: "This email address is already being used"
-                        })
+                        errMsg = "This email address is already being used"
                     } else {
                         // This email is not registered yet
                         let crypto = require('crypto');
@@ -35,17 +48,23 @@ router.post('/', (req, res) => {
                                 throw error
                             } else {
                                 // Sign up successful
-                                res.redirect('/login')
+                                res.render('pages/login', {
+                                    NavEnabled: false,
+                                    ErrMsgEnabled: false,
+                                    errMsg: ""
+                                });
                             }
                         })
                     }
                 }
             })
         } else {
-            // Password and Confirm password does not match
+            //Display error message
             res.render('pages/signup', {
-                message: "Password doesn't match"
-            })
+                NavEnabled: false,
+                ErrMsgEnabled: true,
+                errMsg: errMsg
+            });
         }
     } catch (error) {
         console.log(error.message)
