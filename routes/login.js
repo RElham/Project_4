c57093c5d07dcbfe6bbe06ae35e59a6a03b984de
Siripond <goog.mullanu@gmail.com>
2021-03-03 11:48:02 +1100

    Update validation

--- routes/login.js
+++ routes/login.js
@@ -16,7 +16,7 @@ router.post('/', (req, res) => {
         const { email, password } = req.body;
 
         // Finds the validation errors
-        /*if (validator.isEmpty(email)) {
+        if (validator.isEmpty(email)) {
             res.render('pages/login', {
                 NavEnabled: false,
                 ErrMsgEnabled: true,
@@ -28,38 +28,38 @@ router.post('/', (req, res) => {
                 ErrMsgEnabled: true,
                 errMsg: "Please enter the password"
             });
-        } else if (validator.isEmail(email)) {
+        } else if (!validator.isEmail(email)) {
             res.render('pages/login', {
                 NavEnabled: false,
                 ErrMsgEnabled: true,
                 errMsg: "Please enter email in correct format"
             });
-        }*/
-    
-        let crypto = require('crypto');
-        let pwdEncrypt = crypto.createHash('sha256').update(password).digest('base64');
-        
-        // Check email and password in database
-        let sql = "SELECT * FROM users WHERE email = '" + email + "' AND password = '" + pwdEncrypt + "'"
-        pool.query(sql, (error, results) => {
-            if (error) {
-                throw error
-            } else {
-                if (results.rows.length > 0) {
-                    // Username and password are match: Login successful
-                    let user_id = results.rows[0].user_id
-                    req.session.user_id = user_id;
-                    res.redirect('/home')
+        } else {
+            let crypto = require('crypto');
+            let pwdEncrypt = crypto.createHash('sha256').update(password).digest('base64');
+            
+            // Check email and password in database
+            let sql = "SELECT * FROM users WHERE email = '" + email + "' AND password = '" + pwdEncrypt + "'"
+            pool.query(sql, (error, results) => {
+                if (error) {
+                    throw error
                 } else {
-                    // Username or password doesn't match: Login fail
-                    res.render('pages/login', {
-                        NavEnabled: false,
-                        ErrMsgEnabled: true,
-                        errMsg: "Username or password doesn't match"
-                    });
+                    if (results.rows.length > 0) {
+                        // Username and password are match: Login successful
+                        let user_id = results.rows[0].user_id
+                        req.session.user_id = user_id;
+                        res.redirect('/home')
+                    } else {
+                        // Username or password doesn't match: Login fail
+                        res.render('pages/login', {
+                            NavEnabled: false,
+                            ErrMsgEnabled: true,
+                            errMsg: "Username or password doesn't match"
+                        });
+                    }
                 }
-            }
-        })
+            })
+        }
     } catch (error) {
         console.log(error.message)
     }
